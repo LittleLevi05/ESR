000bcf2ab7ab3f6267cc9c54477c78e13861c58b
jpdiasfernandes <jpdiasfernandes13@gmail.com>
2022-11-24 12:18:22 +0000

    generalize program params

--- client/Cliente.py
+++ client/Cliente.py
@@ -1,18 +1,33 @@
 import sys
 from tkinter import Tk
 from Client import Client
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog = 'Cliente',
+    description = 'TP2 ESR',
+    epilog = 'Made by Henrique and José'
+)
+
+parser.add_argument('ipServer', type=str, help="IP of the multimedia content server")
+parser.add_argument('portServer', type=int, help="Port of the multimedia content server")
+parser.add_argument('rtpPort', type=int, help="Port of the multimedia server that is running")
+
+args, unknown = parser.parse_known_args()
+
 
 if __name__ == "__main__":
 	try:
-		addr = '127.0.0.1'
-		port = 25000
+		addr = args.ipServer
+		port = args.portServer
+		rtpPort = args.rtpPort
 	except:
 		print("[Usage: Cliente.py]\n")
 
 	root = Tk()
 
 	# Create a new client
-	app = Client(root, addr, port, 23500, "movie.Mjpeg")
+	app = Client(root, addr, port, rtpPort, "movie.Mjpeg")
 	app.master.title("Cliente Exemplo")
 	root.mainloop()
 
--- server/Server.py
+++ server/Server.py
@@ -2,11 +2,27 @@ import sys, socket
 
 from ServerWorker import ServerWorker
 
+import argparse
+
+
+parser = argparse.ArgumentParser(
+    prog = 'Server',
+    description = 'TP2 ESR',
+    epilog = 'Made by Henrique and José'
+)
+
+parser.add_argument('portServer', type=int, help="Port of the multimedia content server")
+
+
+args, unknown = parser.parse_known_args()
+
+
 class Server:
+
 
 	def main(self):
 		try:
-			SERVER_PORT = int(sys.argv[1])
+			SERVER_PORT = args.portServer
 		except:
 			print("[Usage: Server.py Server_port]\n")
 		rtspSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
